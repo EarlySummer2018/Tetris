196d996b67ab6fe9af58f5afae786d0dc21a7abb
qwer <huangxiangrui2020@163.com>
2020-09-11 17:22:11 +0800

    tetrisGame

---
+++ js/gameAndMusic.js
@@ -0,0 +1,34 @@
+// 定义定时器全局变量
+var id;
+
+// x向下运动的频率
+
+var intercal = 1000;
+
+// 开始或继续游戏函数
+function go() {
+
+    // 如果当前的文字为 【开始游戏】那么点击之后首先需要刷新分数为 0
+    if (document.querySelector('#action').innerHTML === '开始游戏') {
+        if (map.myScore === 0) {
+            document.querySelector('.score').innerHTML = 0
+        }
+    }
+    //每隔一秒钟渲染最新场地数据
+    // 这个返回的 id 值是在清除定时器的时候使用的
+    id = setInterval(function() {
+
+        // 触底检测是和自动向下的频率是相同的
+
+        //清除砖块
+
+        // 可移动砖块检测
+
+        // 刷新得分记录
+
+        // 自动向下移动一次
+
+        // 重新渲染一次
+        
+    }, 1000)
+}
---
+++ js/style.js
@@ -0,0 +1,74 @@
+class Map {
+    // 设置格子大小为30 像素
+    size = 30
+
+    //设置格子间隙为 5 像素
+    margin = 5
+
+    myScore = 0
+    //创建格子元素列表
+
+    // 注意： 这里的blocks数组里面存放的是div元素
+    // 访问这个数组的时候通过this.blocks[xx][yy]进行访问
+
+    // 会根据 tetris.js 中同名的blocks数组中相应的位置的数字来判断该位置 的div应该是什么颜色，
+    blocks = []
+
+    //定义方法
+    // 封装显示场地的方法
+
+    show() {
+        var box = $('#box');
+
+        // 动态创建div代表场地
+        var map = $('<div></div>')
+            .width(13 * this.size + 12 * this.margin)
+            .height(20 * this.size + 19 * this.margin)
+            // 设置样式
+            .css({
+                backgroundColor: 'lightgreen',
+                position: 'relative',
+                margin: '0 auto'
+            })
+            .appendTo(box)
+        for (let y = 0; y < 20; y++) {
+            this.blocks.push([])
+            for (let x = 0; x < 13; x++) {
+                this.blocks[y].push($('<div></div>')
+                    .width(this.size)
+                    .height(this.size)
+                    .css({
+                        backgroundColor: 'white',
+                        position: 'absolute'
+                    })
+                    .offset({
+                        left: x * (this.size + this.margin),
+                        top: y * (this.size + this.margin)
+                    })
+                    .appendTo(map))
+            }
+        }
+    }
+
+    // 封装砖块渲染的方法
+
+    render() {
+        // 遍历砖块数据，狗日对应的砖块div设置颜色
+        // 可移动方块为红色 1， 固定方块为蓝色 2，
+        for (let y = 0; y < 20; y++) {
+            for (let x = 0; x < 13; x++) {
+                switch (blocks[y][x]) {
+                    case 0:
+                        this.blocks[y][x].css('backgroundColor', 'white');
+                        break;
+                    case 1:
+                        this.blocks[y][x].css('backgroundColor', 'red');
+                        break;
+                    case 2:
+                        this.blocks[y][x].css('backgroundColor', 'blue');
+                        break;
+                }
+            }
+        }
+    }
+}
